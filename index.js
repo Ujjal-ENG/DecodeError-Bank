470b64ad9fe84b8aeff007f739a25446ffe89835
user <ujjalroy7862@gmail.com>
2022-08-14 16:07:11 +0600

    Decode-Error-Bank project done new

--- index.js
+++ index.js
@@ -15,27 +15,21 @@ depostiBtn.addEventListener("click", () => {
   let value = document.getElementById("depositInput").value;
   let intValue = parseInt(value);
 
-  let currentAmount = document.getElementById("currentDeposit").innerText;
-  let intCurrentAmount = parseInt(currentAmount);
+  updateSpanText("currentDeposit", intValue);
+  updateSpanText("currentBalance", intValue);
 
-  let currentBalance = document.getElementById("currentBalance").innerText;
-  let intCurrentBalance = parseInt(currentBalance);
-
-
-  let result = intValue + intCurrentAmount;
-  let strResult = String(result);
-  document.getElementById("currentDeposit").innerText = strResult;
-
-  let balnce = intCurrentBalance + intValue;
-  let strBalnce = String(balnce);
-  document.getElementById("currentBalance").innerText = strBalnce;
-  
-  document.getElementById("depositInput").value = ""
-  
+  document.getElementById("depositInput").value = "";
 });
 
-withdrawBtn.addEventListener("click", () => {
+function updateSpanText(id, intValue) {
+  let currentAmount = document.getElementById(id).innerText;
+  let intCurrentAmount = parseInt(currentAmount);
+  let result = intValue + intCurrentAmount;
+  let strResult = String(result);
+  document.getElementById(id).innerText = strResult;
+}
 
+withdrawBtn.addEventListener("click", () => {
   let value = document.getElementById("withdrawInput").value;
 
   console.log(value);
@@ -44,22 +38,22 @@ withdrawBtn.addEventListener("click", () => {
 
   let currentWithdraw = document.getElementById("currentWithdraw").innerText;
 
- 
-
   let intCurrentBalance = parseInt(currentBalance);
-  let intValue = parseInt(value)
+  let intValue = parseInt(value);
 
-  console.log(intCurrentBalance)
-  
+  console.log(intCurrentBalance);
+
   let intCurrentWithdraw = parseInt(currentWithdraw) + intValue;
-  document.getElementById("currentWithdraw").innerText = String(intCurrentWithdraw)
+  document.getElementById("currentWithdraw").innerText =
+    String(intCurrentWithdraw);
 
-  console.log(intCurrentWithdraw)
+  console.log(intCurrentWithdraw);
 
-  let currentAmount = intCurrentBalance - intCurrentWithdraw
-console.log(currentAmount)
-  
-  let result = String(currentAmount)
-  document.getElementById("currentBalance").innerText = result
-  document.getElementById("withdrawInput").value = ""
+  let currentAmount = intCurrentBalance - intCurrentWithdraw;
+  console.log(currentAmount);
+
+  let result = String(currentAmount);
+  document.getElementById("currentBalance").innerText = result;
+  document.getElementById("withdrawInput").value = "";
 });
+
